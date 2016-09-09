f4a017f0b46fe04b4a13dc0884f57f6b44fd6bf1
Fermatt98 <matiasfernandez602@gmail.com>
2016-09-08 22:49:42 -0300

    Ahora si!

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,13 +9,31 @@ import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	private var player:FlxSprite;
 	override public function create():Void
 	{
 		super.create();
+		player = new FlxSprite();
+		player.makeGraphic(16, 8);
+		player.y = FlxG.height - player.height;
+		player.x = 80 - player.width/2;
+		add(player);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		if (FlxG.keys.pressed.LEFT && player.x > 0)
+		{
+			player.velocity.x = -Reg.velPlayer;
+		}
+		else if (FlxG.keys.pressed.RIGHT && player.x < FlxG.width-player.width)
+		{
+			player.velocity.x = Reg.velPlayer;
+		}
+		else
+		{
+			player.velocity.x = 0;
+		}
 	}
 }
---
+++ source/Reg.hx
@@ -0,0 +1,14 @@
+package;
+
+class Reg
+{
+		static public var highscore:Int = 50000;
+		static public var cantVidas:Int = 3;
+		static public var nombreHighscore:String = "CPU";
+		
+		inline static public var maxCantVidas:Int = 3;
+		inline static public var velDisparo:Int = 200;
+		inline static public var velPlayer:Int = 40;
+		inline static public var puntosEnemigo:Int = 100;
+		inline static public var puntosOvni:Int = 1000;
+}
