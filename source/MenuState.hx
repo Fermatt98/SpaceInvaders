c776361c6eea10e9bc002b07e0f9997e5e2cff21
gonzaloElMasPiola <zeiss.gonzalo@cts.edu.ar>
2016-09-15 16:54:28 -0700

    a las chicas de verdad les gusta el pollo frito pollo pollo POLLAAAAAA

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,13 +9,22 @@ import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
+	private var menu:FlxSprite;
 	override public function create():Void
 	{
 		super.create();
+		menu = new FlxSprite();
+		menu.makeGraphic(160, 144);
+		menu.loadGraphic("assets/img/gif/menu.png");
+		add(menu);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		if (FlxG.keys.justPressed.SPACE)
+		{
+			FlxG.switchState(new PlayState());
+		}	
 	}
 }
