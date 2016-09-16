94ce2eec35b29ed857fb5c18144f6e080b6c39f2
gonzaloElMasPiola <zeiss.gonzalo@cts.edu.ar>
2016-09-15 21:38:24 -0700

    ghhhhhhh ghhhhhhh LUCK soy tu papichulo

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -10,20 +10,26 @@ import flixel.math.FlxMath;
 class MenuState extends FlxState
 {
 	private var menu:FlxSprite;
+	private var time:Float;
 	override public function create():Void
 	{
 		super.create();
 		menu = new FlxSprite();
 		menu.makeGraphic(160, 144);
-		menu.loadGraphic("assets/img/gif/menu.png");
+		menu.loadGraphic("assets/img/gif/previa.png");
 		add(menu);
-
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		if (FlxG.keys.justPressed.SPACE)
+		time = time + elapsed;
+		trace(time);
+		if (time >= 9)
+		{
+			menu.loadGraphic("assets/img/gif/menu.png");
+		}
+		if (FlxG.keys.justPressed.SPACE && time > 10)
 		{
 			FlxG.switchState(new PlayState());
 		}	
